faaebe930006ed6fb7655506db27c5db1bac8d93
bradsta <bradstarsca@gmail.com>
2017-01-21 11:20:08 -0800

    Player & movement stuff

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    Rigidbody2D player;
+    Rigidbody2D gun;
+    public GameObject bullet;
+
+    public Vector2 velocity;
+
+    private Animator animator;
+
+    private byte state = 0; //Idle
+
+    // Use this for initialization
+    void Start() {
+        player = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        //gun = gameObject.transform.GetChild(0).GetComponent<Rigidbody2D>();
+    }
+
+
+    public void move(float directionx,float directiony)
+    {
+        if (directionx == 0 && directiony == 0) //Not moving
+        {
+            if (state == 1) //Was moving previously
+            {
+                state = 0;
+                animator.Play("Idle");
+            }
+        } else
+        {
+            player.MovePosition(new Vector2(player.position.x + velocity.x * directionx * Time.fixedDeltaTime, player.position.y + velocity.y * directiony * Time.fixedDeltaTime));
+
+            if (state == 0)
+            {
+                state = 1; //Moving
+                animator.Play("Idle");
+            }
+        }
+
+
+        //gun.MovePosition(new Vector2(gun.position.x + velocity.x * directionx * Time.fixedDeltaTime, gun.position.y + velocity.y * directiony * Time.fixedDeltaTime));
+    }
+
+    public void Rotate(int rotate_speed)
+    {
+        gun.transform.RotateAround(player.transform.position, player.transform.forward, rotate_speed*20 * Time.deltaTime);
+        //gun.GetComponent<Rigidbody2D>().MoveRotation(gun.GetComponent<Rigidbody2D>().rotation+ rotate_speed);
+       // gun.GetComponent<Rigidbody2D>().
+    }
+
+    public void Shoot()
+    {
+        //Instantiate(bullet, new Vector2(gun.position.x,gun.position.y), gun.transform.localRotation);
+
+    }
+}
+
