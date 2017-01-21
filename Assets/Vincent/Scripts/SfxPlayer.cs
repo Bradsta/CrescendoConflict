bc50251795831a3f8fafa820fba4cbf2d7484e77
VinCMG <vincentchang9999@gmail.com>
2017-01-20 23:27:03 -0800

    Initial SfxPlayer work
    I haven't tested anything yet.

---
+++ Assets/Vincent/Scripts/SfxPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SfxPlayer : MonoBehaviour {
+	public AudioClip[] soundEffects;
+
+	private AudioSource audioSource;
+	public Dictionary<string, AudioClip> soundEffectsDict;
+
+	// Use this for initialization
+	void Start () {
+		GameObject child = new GameObject("AudioSource");
+		child.transform.parent = gameObject.transform;
+		audioSource = child.AddComponent<AudioSource>();
+
+		// create dictionary from soundEffects
+		foreach (AudioClip clip in soundEffects) {
+			Debug.Log("adding " + clip.name + " to soundEffectsDict");
+			soundEffectsDict[clip.name] = clip;
+		}
+	}
+
+	void playSoundEffect(string name, float volume = 1.0f) {
+		audioSource.PlayOneShot(soundEffectsDict[name], volume);
+	}
+}
