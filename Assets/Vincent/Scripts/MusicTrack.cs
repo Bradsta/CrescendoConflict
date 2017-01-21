e2fe129e9ff72aaa91911d921cef8a558eb4f2b3
VinCMG <vincentchang9999@gmail.com>
2017-01-20 22:20:05 -0800

    Working background loop music player

---
+++ Assets/Vincent/Scripts/MusicTrack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicTrack : MonoBehaviour {
+
+	public MusicClip[] clips; 
+
+	private int currentClip = 0;
+	private bool started = false;
+
+	// Use this for initialization
+	void Start () {
+		started = false;
+		clips = GetComponentsInChildren<MusicClip>();
+	}
+
+	public MusicClip getNextClip() {
+		if (!started) {			// All clip playback algorithms start from index 0 initially
+			currentClip = 0;
+			started = true;
+		} else if (clips.Length > 1) {
+			currentClip = (currentClip  + 1) % clips.Length;
+		}
+
+		return clips[currentClip];
+	}
+}
+
+
