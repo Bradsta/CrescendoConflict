78e74cca04c8708f278ba27655317f4771093c9e
Ittai Mann <manni@uci.edu>
2017-01-20 22:17:31 -0800

    changed input again
    changed input again

--- Assets/ittai/inputHandler.cs
+++ Assets/ittai/inputHandler.cs
@@ -18,15 +18,15 @@ public class inputHandler : MonoBehaviour {
 
     }
 
-    public void moveHorz(int direction)
+    public void move(float directionx,float directiony)
     {
-        player.MovePosition(new Vector2(player.position.x + velocity.x * direction * Time.fixedDeltaTime, player.position.y));
-                                 // player.position.y + velocity.y * Input.GetAxisRaw("Vertical") * Time.fixedDeltaTime));
+        player.MovePosition(new Vector2(player.position.x + velocity.x * directionx * Time.fixedDeltaTime, player.position.y + velocity.y *directiony * Time.fixedDeltaTime));
+
     }
-    public void moveVert(int direction)
+  /*  public void moveVert(float direction)
     {
         player.MovePosition(new Vector2(player.position.x,player.position.y + velocity.y * direction * Time.fixedDeltaTime));//player.position.x + velocity.x * direction * Time.fixedDeltaTime,
 
-    }
+    }*/
 }
 
--- Assets/ittai/inputPasser.cs
+++ Assets/ittai/inputPasser.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class inputPasser : MonoBehaviour {
 
-    //Rigidbody2D player;
-    //public Vector2 velocity;
-    public float rotate_speed;
-
-    public string moveUp;
-    public string moveDown;
-    public string moveLeft;
-    public string moveRight;
     // Use this for initialization
     void Start () {
       //  player = gameObject.GetComponent<Rigidbody2D>();
@@ -20,22 +12,7 @@ public class inputPasser : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-        if (Input.GetKey(moveUp))
-        {
-            gameObject.GetComponent<inputHandler>().moveVert(1);
-        }
-        if (Input.GetKey(moveDown))
-        {
-            gameObject.GetComponent<inputHandler>().moveVert(-1);
-        }
-        if (Input.GetKey(moveLeft))
-        {
-            gameObject.GetComponent<inputHandler>().moveHorz(-1);
-        }
-        if (Input.GetKey(moveRight))
-        {
-            gameObject.GetComponent<inputHandler>().moveHorz(1);
-        }
+        gameObject.GetComponent<inputHandler>().move(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
 
 
