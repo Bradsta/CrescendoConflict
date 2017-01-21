c5694f3e05d7213e59f086ed32c5bf74278e02f7
Ittai Mann <manni@uci.edu>
2017-01-20 22:10:45 -0800

    changed movement
    Slight change to movement script

---
+++ Assets/ittai/inputHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputHandler : MonoBehaviour {
+
+    Rigidbody2D player;
+    public Vector2 velocity;
+
+    // Use this for initialization
+    void Start() {
+        player = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void moveHorz(int direction)
+    {
+        player.MovePosition(new Vector2(player.position.x + velocity.x * direction * Time.fixedDeltaTime, player.position.y));
+                                 // player.position.y + velocity.y * Input.GetAxisRaw("Vertical") * Time.fixedDeltaTime));
+    }
+    public void moveVert(int direction)
+    {
+        player.MovePosition(new Vector2(player.position.x,player.position.y + velocity.y * direction * Time.fixedDeltaTime));//player.position.x + velocity.x * direction * Time.fixedDeltaTime,
+
+    }
+}
+
---
+++ Assets/ittai/inputPasser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputPasser : MonoBehaviour {
+
+    //Rigidbody2D player;
+    //public Vector2 velocity;
+    public float rotate_speed;
+
+    public string moveUp;
+    public string moveDown;
+    public string moveLeft;
+    public string moveRight;
+    // Use this for initialization
+    void Start () {
+      //  player = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKey(moveUp))
+        {
+            gameObject.GetComponent<inputHandler>().moveVert(1);
+        }
+        if (Input.GetKey(moveDown))
+        {
+            gameObject.GetComponent<inputHandler>().moveVert(-1);
+        }
+        if (Input.GetKey(moveLeft))
+        {
+            gameObject.GetComponent<inputHandler>().moveHorz(-1);
+        }
+        if (Input.GetKey(moveRight))
+        {
+            gameObject.GetComponent<inputHandler>().moveHorz(1);
+        }
+
+
+
+    }
+}
