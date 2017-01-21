13b5b4aa40349c4246d80d4b08df5bf375eee9a2
Ittai Mann <manni@uci.edu>
2017-01-20 23:22:52 -0800

    added bullets
    added bullets, shooting not working yet

---
+++ Assets/ittai/scripts/shoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    Rigidbody2D barrel;
+	// Use this for initialization
+	void Start () {
+        barrel = GameObject.FindGameObjectWithTag("GUN").GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+}
