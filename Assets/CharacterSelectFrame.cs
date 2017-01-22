adc07dfaa17081005f44ca8ef120521fa921654a
sowrd299 <ryan.ward46@gmail.com>
2017-01-21 17:58:37 -0800

    Misc Updates to Character Select

---
+++ Assets/CharacterSelectFrame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectFrame : MonoBehaviour {
+
+    Image character;
+
+	// Use this for initialization
+	void Start () {
+        character = transform.Find("Character").GetComponent<Image>();
+        character.gameObject.SetActive(false);
+	}
+
+    public void SetDisp (Sprite sprite) {
+        character.gameObject.SetActive(true);
+        character.sprite = sprite;
+    }
+	
+}
--- Assets/khang/CharacterSelect.cs
+++ Assets/khang/CharacterSelect.cs
@@ -6,37 +6,54 @@ public class CharacterSelect : MonoBehaviour {
     public Sprite[] characterImages;
     //need to map each player to 1 index in this (list? map?)
     //stores the index for which character the player has selected
+
     // var for storing the currently selected character
-    public int selectedCharacter = 0;
+    private int selectedCharacter = 0;
 	// Use this for initialization
 
 	void Start () {
         GameVars.PlayerCount = 0; 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
-    void ScrollUp()
+
+    void Update() {
+        
+    }
+ 
+    public void ScrollUp()
     {
         selectedCharacter--;
         //if it's about to hit less than 0, go back to the end
         if (selectedCharacter - 1 < 0)
             selectedCharacter = characterImages.Length - 1;
+        updateDisp();
     }
 
-    void ScrollDown()
+    public void ScrollDown()
     {
         selectedCharacter++;
         //if it's about to be greater than the character list size, go back to the beginning
         if (selectedCharacter + 1 > characterImages.Length - 1)
             selectedCharacter = 0;
+        updateDisp();
     }
 
-    void Progress() {
-        //save the current player and move on
-        ++GameVars.PlayerCount;
+    void updateDisp() {
+        transform.GetChild(GameVars.PlayerCount).GetComponent<CharacterSelectFrame>().SetDisp(characterImages[selectedCharacter]);
+    } 
+
+    public void Progress() {
+        //save the current character and move on
+        GameVars.Avatars[GameVars.PlayerCount] = (GameVars.Avatar)selectedCharacter;
+        if (GameVars.PlayerCount <= transform.childCount) { //use number of frames as indication of max number of characters 
+            ++GameVars.PlayerCount;
+            updateDisp();
+        }else {
+            //if out of frames, begin games
+            BeginGame();
+        }
+    }
+
+    public void BeginGame() {
+        //begin the game
     }
 }
